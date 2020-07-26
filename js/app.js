50257ad376dbea2e9f004bce9e203c505932f392
Gianvito Difilippo <gianvito.difilippo@gmail.com>
2020-07-26 17:02:19 +0200

    test

--- js/app.js
+++ js/app.js
@@ -5,6 +5,8 @@ const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a'
 const header = document.querySelector('.header.container');
 const brand = document.querySelector('.brand h1');
 
+alert('This portfolio is work in progress');
+
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobile_menu.classList.toggle('active');

d5d4fcdbdcc6ae94c9071e3160985fddd1a00605
Gianvito Difilippo <gianvito.difilippo@gmail.com>
2020-09-02 20:20:10 +0200

    skill select animation mobile

--- js/index_mobile.js
+++ js/index_mobile.js
@@ -2,12 +2,37 @@ function toggleSkillMobile(skill) {
     body.classList.toggle('stop_scroll');
     Array.from(document.getElementsByTagName('section'))
         .forEach(item => {
-            item.style.filter = 'blur(0px)';
-            item.style.transition = 'filter 3s';
-            setTimeout(() => item.style.filter = 'blur(4px)', 1000);
+            item.animate([
+                {
+                    filter: 'blur(0px)'
+                },
+                {
+                    filter: 'blur(4px)'
+                }
+            ], { duration: 200, fill: 'forwards' });
         });
+    setTimeout(function() {
+        skill_preview.style.display = null;
+        skill_preview.animate([
+            {
+                opacity: 0,
+                transform: 'scale(0)'
+            },
+            {
+                opacity: 1,
+                transform: 'scale(1)'
+            }
+        ], { duration: 200 })
+    }, 100);
     skill_preview_popup.style.display = null;
-    skill_preview.style.display = 'initial';
+    skill_preview_popup.animate([
+        {
+            opacity: 0
+        },
+        {
+            opacity: 1
+        }
+    ], { duration: 200 });
     skill_preview_popup.appendChild(skill_preview);
     setSkill(skill);
 }
@@ -28,5 +53,5 @@ function indexMobile()
     // Toggles navlist when clicking on list items
     menu_items.forEach(item => item.addEventListener('click', toggleNavlistActive));
 
-    openFullscreen();
+    skill_preview.style.display = 'none';
 }

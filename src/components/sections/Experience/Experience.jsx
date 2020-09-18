fd5beea88002d3f75aee82b0cbc6a8888e400dc3
Gianvito Difilippo <gianvito.difilippo@gmail.com>
2020-09-18 13:38:16 +0200

    fixes

--- src/components/sections/Experience/Experience.jsx
+++ src/components/sections/Experience/Experience.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Section from '../Section';
 
+import Translate from '../../misc/Translate';
+
 import './experience_desktop.scss';
 import './experience_tablet.scss';
 import './experience_phone.scss';
@@ -10,61 +12,73 @@ import backend from '../../../assets/img/experience/backend.png';
 import truck from '../../../assets/img/experience/truck.png';
 import tutor from '../../../assets/img/experience/tutor.png';
 
+const TimelineItem = props => (
+    <li className="timeline-item">
+        <div className="hexagon">
+            <div className="hexagon-content">
+                <img src={props.image} alt="" className="hexagon-image"/>
+            </div>
+        </div>
+        <h2><Translate selector={`experience:${props.id}:year`}>{props.id}</Translate></h2>
+        <div className="box">
+            <h1 className="box-heading"><Translate selector={`experience:${props.id}:name`}>{props.id}</Translate></h1>
+            <h3>@ {props.where}</h3>
+        </div>
+    </li>
+);
+
 class Experience extends React.PureComponent
 {
+    constructor(props)
+    {
+        super(props);
+
+        this.experience = [
+            {
+                id: 'research',
+                where: 'Politecnico di Bari',
+                image: truck
+            },
+            {
+                id: 'tutoring',
+                where: 'Politecnico di Bari',
+                image: tutor
+            },
+            {
+                id: 'backend',
+                where: 'AutoLogS',
+                image: backend
+            },
+            {
+                id: 'software',
+                where: 'Code Architects',
+                image: backend
+            }
+        ];
+
+        this.ulClassName = this.ulClassName.bind(this);
+    }
+
+    ulClassName()
+    {
+        let className = 'timeline';
+        if (this.experience.length % 2) {
+            className += ' odd-nli';
+        }
+        else {
+            className += ' even-nli';
+        }
+        return className;
+    }
+
     render()
     { 
         return (
             <Section id="experience" defaultTitle="Esperienza">
-                <ul className="timeline">
-                    <li className="timeline-item">
-                        <div className="hexagon">
-                            <div className="hexagon-content">
-                                <img src={truck} alt="" className="hexagon-image"/>
-                            </div>
-                        </div>
-                        <h2>2017-2018</h2>
-                        <div className="box">
-                            <h1 className="box-heading">Research Assistant</h1>
-                            <h3>@ Politecnico di Bari</h3>
-                        </div>
-                    </li>
-                    <li className="timeline-item">
-                        <div className="hexagon">
-                            <div className="hexagon-content">
-                                <img src={tutor} alt="" className="hexagon-image"/>
-                            </div>
-                        </div>
-                        <h2>2018-2019</h2>
-                        <div className="box">
-                            <h1 className="box-heading">Student's Tutor</h1>
-                            <h3>@ Politecnico di Bari</h3>
-                        </div>
-                    </li>
-                    <li className="timeline-item">
-                        <div className="hexagon">
-                            <div className="hexagon-content">
-                                <img src={backend} alt="" className="hexagon-image"/>
-                            </div>
-                        </div>
-                        <h2>2020</h2>
-                        <div className="box">
-                            <h1 className="box-heading">.NET Developer</h1>
-                            <h3>@ Autologs</h3>
-                        </div>
-                    </li>
-                    <li className="timeline-item">
-                        <div className="hexagon">
-                            <div className="hexagon-content">
-                                <img src={backend} alt="" className="hexagon-image"/>
-                            </div>
-                        </div>
-                        <h2>2020-Present</h2>
-                        <div className="box">
-                            <h1 className="box-heading">Software Developer</h1>
-                            <h3>@ Code Architects</h3>
-                        </div>
-                    </li>
+                <ul className={this.ulClassName()}>
+                {this.experience.map(value => (
+                    <TimelineItem key={value.id} id={value.id} where={value.where} image={value.image}/>
+                ))}
                 </ul>
             </Section>
         );

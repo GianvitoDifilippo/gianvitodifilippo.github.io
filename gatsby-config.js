346ffe4c58a36863e40dda08bf2fd909a7ec61e3
Gianvito Difilippo <gianvito.difilippo@gmail.com>
2022-05-30 18:27:24 +0200

    Fix images

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,14 +14,15 @@
         name: 'Gianvito Difilippo',
         short_name: 'Gianvito Difilippo',
         start_url: '/',
+        icon: 'src/assets/favicon.png',
         icons: [
           {
-            src: `/icons/manifest-icon-192.maskable.png`,
+            src: `icons/manifest-icon-192.maskable.png`,
             sizes: `192x192`,
             type: `image/png`,
           },
           {
-            src: `/icons/manifest-icon-512.maskable.png`,
+            src: `icons/manifest-icon-512.maskable.png`,
             sizes: `512x512`,
             type: `image/png`,
           },

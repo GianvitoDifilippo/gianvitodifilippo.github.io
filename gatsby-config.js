f299fb989dc6bb628b91786c80a47c85aaa048ac
Gianvito Difilippo <gianvito.difilippo@gmail.com>
2022-05-30 17:55:43 +0200

    Add pwa support

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
-module.exports = {
+ module.exports = {
   plugins: [
     'gatsby-plugin-sass',
     'gatsby-plugin-react-helmet',
@@ -13,8 +13,22 @@ module.exports = {
       options: {
         name: 'Gianvito Difilippo',
         short_name: 'Gianvito Difilippo',
-        icon: 'src/assets/favicon.png'
+        start_url: '/',
+        icons: [
+          {
+            src: `/icons/manifest-icon-192.maskable.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/icons/manifest-icon-512.maskable.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+        display: 'fullscreen'
       }
-    }
+    },
+    'gatsby-plugin-offline'
   ],
 }
